b40f7f519422e8e5063b3baec9a14decc5b2e17a
zhanggang <zhanggang2019@boco.com.cn>
2019-07-24 14:17:51 +0800

    添加Ocelot Api网关 相关代码（未涉及idrs4）

---
+++ WebApiA/Controllers/CounterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiA.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CounterController : ControllerBase
+    {
+        private static int _count = 0;
+
+        [HttpGet]
+        public string Count()
+        {
+            return $"count {++_count} from WebApiA";
+        }
+    }
+}
---
+++ WebApiA/Controllers/SlowController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiA.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SlowController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> GetName()
+        {
+            await Task.Delay(6000);
+            return "Jonathan";
+        }
+    }
+}
